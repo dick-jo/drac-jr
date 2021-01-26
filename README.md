2ab041903af940377a6140969feec543668bdb05
Dick Jones <jones.rrw@gmail.com>
2021-01-26 18:58:58 +1030

    Add README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# Drac Jr.
+An itty bitty dark mode handler
+
+Drac Jr works via CSS Data Attributes allowing for dark mode styling using the `[data-theme=dark]` selector
+
+## Quick install:
+
+`npm install drac-jr`
+
+## How to use:
+### 0. import DracJr
+    import DracJr, { ToggleDark } from "drac-jr"
+### 1. Run DracJr() somewhere sensible
+    App = () => {
+        DracJr()
+        
+        return ...
+    
+`DracJr()` runs an initial check to see whether the user has previously saved a preference with you.
+...if not, the `(prefers-color-scheme: dark)` query is checked and OS/browser level preferencee applied
+
+### 2. Add a light/dark toggle
+    <button onClick={ToggleDark}>Dark Mode</button>
+
+Every time a toggle is triggered, preference is saved via `localStorage.setItem()`
+    
--- drac-jr.js
+++ drac-jr.js
@@ -1,39 +1,42 @@
-const prefersDark = window.matchMedia("(prefers-color-scheme: dark)")
-const body = document.body
-const savedTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null
+// Just run DracJr() to perform the initial check + set
+const DracJr = () => {
+    // Initial check for previously indicated dark/light preference
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)")
+        // ...if user has never indicated preference here before, checks OS/Browser for dark/light preference
+    const savedTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null
 
-// Check for previously indicated dark/light preference
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme)
 
-if (savedTheme) {
-    document.documentElement.setAttribute('data-theme', savedTheme)
-} else {
-    // Check for system preference and save for future
-    // ...currently somewhat of a workaround to avoid flash of unstyled content on page load for dark users
-    window.onload = setDataTheme = () => {
-        if (prefersDark.matches) {
-            document.documentElement.setAttribute('data-theme', 'dark')
-            localStorage.setItem('theme', 'dark')
-            console.log("Contrast mode " + document.documentElement.getAttribute('data-theme'))
-        } else {
-            document.documentElement.setAttribute('data-theme', 'light')
-            localStorage.setItem('theme', 'light')
-            console.log("Contrast mode set to " + document.documentElement.getAttribute('data-theme'))
+    } else {
+        // Check for system preference and save for future
+        // ...currently somewhat of a workaround to avoid flash of unstyled content on page load for dark users
+        const setDataTheme = () => {
+            if (prefersDark.matches) {
+                document.documentElement.setAttribute('data-theme', 'dark')
+                localStorage.setItem('theme', 'dark')
+            } else {
+                document.documentElement.setAttribute('data-theme', 'light')
+                localStorage.setItem('theme', 'light')
+            }
         }
+
+        window.onload = setDataTheme()
     }
 }
 
 // Handle toggle via click
 
-const btn = document.querySelector(".toggle-dark")
+export const toggleDark = () => {
+    console.log("toggle triggered")
 
-btn.addEventListener("click", () => {
     if (document.documentElement.getAttribute('data-theme') === 'dark') {
         document.documentElement.setAttribute('data-theme', 'light')
         localStorage.setItem('theme', 'light')
-        console.log("Contrast mode set to " + document.documentElement.getAttribute('data-theme'))
     } else {
         document.documentElement.setAttribute('data-theme', 'dark')
         localStorage.setItem('theme', 'dark')
-        console.log("Contrast mode set to " + document.documentElement.getAttribute('data-theme'))
     }
-})
+}
+
+export default DracJr
