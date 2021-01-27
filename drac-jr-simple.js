8e6428cad730311525dbdd7a379e4f0e13bbddc6
Dick Jones <jones.rrw@gmail.com>
2021-01-27 21:52:28 +1030

    Add import/exportless version

---
+++ drac-jr-simple.js
@@ -0,0 +1,40 @@
+// Identical to drac-jr.js, but without exports/imports for absolutely simplicity
+
+console.log("pre drac")
+const DracJr = () => {
+    console.log("Drac initiating")
+        // Initial check for previously indicated dark/light preference
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)"); // ...if user has never indicated preference here before, checks OS/Browser for dark/light preference
+
+    const savedTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-theme', savedTheme);
+    } else {
+        // Check for system preference and save for future
+        // ...currently somewhat of a workaround to avoid flash of unstyled content on page load for dark users
+        const setDataTheme = () => {
+            if (prefersDark.matches) {
+                document.documentElement.setAttribute('data-theme', 'dark');
+                localStorage.setItem('theme', 'dark');
+            } else {
+                document.documentElement.setAttribute('data-theme', 'light');
+                localStorage.setItem('theme', 'light');
+            }
+        };
+
+        window.onload = setDataTheme()
+    }
+}; // Handle toggle via click
+
+
+const ToggleDark = () => {
+    console.log("toggle")
+    if (document.documentElement.getAttribute('data-theme') === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+};
